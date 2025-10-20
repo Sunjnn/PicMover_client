a2144c93b29022f3a1b343de4318c2f39d8e71fd
亭鹤 <sunjianan.sjn@alibaba-inc.com>
2025-10-20 17:46:35 +0800

    Initial Commit

---
+++ PicMover_client/PicMover_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicMover_clientApp.swift
+//  PicMover_client
+//
+//  Created by sunjnn on 2025/10/20.
+//
+
+import SwiftUI
+
+@main
+struct PicMover_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
